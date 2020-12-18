d3a398d09a18ce14b949940cbd4bce56246787c5
Samvw90 <45743681+Samvw90@users.noreply.github.com>
2020-12-17 17:00:00 -0800

    tweet stream working

--- server/index.js
+++ server/index.js
@@ -1,8 +1,84 @@
+const e = require('express');
 const needle = require('needle');
 const config = require('dotenv').config();
 
 const rulesURL = 'https://api.twitter.com/2/tweets/search/stream/rules';
 const streamURL =
-  'https://api.twitter.com/2/tweets/search/stream?tweet.field=public_metrics&expansions=author_id';
+  'https://api.twitter.com/2/tweets/search/stream?tweet.fields=public_metrics&expansions=author_id';
 
 const rules = [{ value: 'giveaway' }];
+
+//Get stream rules
+async function getRules() {
+  const response = await needle('get', rulesURL, {
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+  });
+  return response;
+}
+
+//POST set rules
+async function setRules() {
+  const data = {
+    add: rules,
+  };
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+  });
+  return response.body;
+}
+
+//DELETE stream rules
+async function deleteRules(rules) {
+  if (!Array.isArray(rules.data)) {
+    return null;
+  }
+  const ids = rules.data.map((rule) => rule.id);
+  const data = {
+    delete: {
+      ids: ids,
+    },
+  };
+  const response = await needle('post', rulesURL, data, {
+    headers: {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+  });
+  return response.body;
+}
+
+function streamTweets() {
+  const stream = needle.get(streamURL, {
+    headers: {
+      Authorization: `Bearer ${process.env.BEARER_TOKEN}`,
+    },
+  });
+
+  stream.on('data', (data) => {
+    try {
+      const json = JSON.parse(data);
+      console.log('JSON: ', json);
+    } catch (error) {}
+  });
+}
+
+(async () => {
+  let currentRules;
+  try {
+    //Get all stream rules
+    currentRules = await getRules();
+    //Delete all stream rules
+    await deleteRules(currentRules);
+    //Set rules based on array above
+    await setRules();
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+  streamTweets();
+})();
